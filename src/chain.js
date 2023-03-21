05793259f9ef85a59d522b0ca4ad6b0119762da3
Petr Porobov <porobov@users.noreply.github.com>
2023-03-21 12:53:55 +0400

    Query events works

--- src/chain.js
+++ src/chain.js
@@ -10,7 +10,7 @@ class MyContract {
     let contract = await ethers.getContractAt(this.contractName, this.contractAddress) 
     const eventFilter = contract.filters[eventName]()
     const events = await contract.queryFilter(eventFilter, fromBlock, 'latest')
-    return events[0].args
+    return events.map(ev => ev.args)  
   }
 }
 
