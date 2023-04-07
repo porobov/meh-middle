8c3bab02bf2def372c0d96e5788f664649e312e7
Petr Porobov <porobov@users.noreply.github.com>
2023-04-07 12:34:00 +0400

    added simplest logger

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { DB } = require("./src/db.js")
 const hre = require("hardhat");
 const { WebGateway } = require("./src/web.js")
 const { ImageEditor } = require("./src/imageEditor.js")
-let db = new DB(hre.config.dbConf)
+const winston = require('winston')
 NEXT_RETRY_DELAY = 1000 * 60 * 5 // 5 minutes
 MAX_NUM_OF_DOWNLOAD_ATTEMPTS = 5
 STATUSCODES_ALLOWING_RETRY = [ 408, 502, 503, 504, 429 ]
@@ -11,58 +11,63 @@ THUMBNAIL_PARAMS = {
     width: 400,
     height: 400
 }
-/*
-let renderer = new Renderer()
-let downloader = new WWW
-*/
+
 function getDimensions(adRecord) {
     return {
         width: (1 + adRecord.toX - adRecord.fromX),
         height: (1 + adRecord.toY - adRecord.fromY)
     }
 }
+
 async function main() {
+    const logger = winston.createLogger({
+        level: 'info',
+        format: winston.format.json(),
+        transports: [new winston.transports.Console()],
+      });
+
     // get latest block
+    let db = new DB(hre.config.dbConf)
     await db.connect()
     let eventName = "NewImage"
     let [ fromBlock, lbError ] = await db.getLatestBlockForEvent(eventName)
-    console.log(`${eventName} event latest block in DB is ${fromBlock}`)
+    logger.info(`${eventName} event latest block in DB is ${fromBlock}`)
     
     // get events
     let contractName = "MillionEther"
     let contractAddress = "0x15dbdB25f870f21eaf9105e68e249E0426DaE916"
     let contract = new MillionEther(contractName, contractAddress)
-    // console.log(`Checking ${eventName} event starting from block ${fromBlock}`)
+    // logger.info(`Checking ${eventName} event starting from block ${fromBlock}`)
     let newEvents = await contract.getEvents(eventName, fromBlock)
-    console.log(`Received ${newEvents.decodedEvents.length} new events till block ${newEvents.blockNumber}`)
+    logger.info(`Received ${newEvents.decodedEvents.length} new events till block ${newEvents.blockNumber}`)
 
     // save new events to db
     if (newEvents.decodedEvents.length > 0) {
         const [ insertsCount, insertError ] = await db.addAds(newEvents.decodedEvents)
-        console.log(`${ insertsCount } new events were written to db`)
+        logger.info(`${ insertsCount } new events were written to db`)
     }
 
     // save block number for event    
     let [savedSuccessfully, saveError] = await db.saveLatestBlockForEvent(eventName, newEvents.blockNumber)
     if (savedSuccessfully) {
-        console.log(`Saved block ${newEvents.blockNumber} for ${eventName} event to db`)
+        logger.info(`Saved block ${newEvents.blockNumber} for ${eventName} event to db`)
     }
 
     // get ads with no images (not downloaded)
     let [ ads, adsLoadError ] = await db.getAdsNoImages()
-    console.log(`Got ${ads.length} ads with no images.`)
+    logger.info(`Got ${ads.length} ads with no images.`)
 
     // download images and save to db
     let wg = new WebGateway()
     for (ad of ads) {
         ad.updates = {}
-        console.log(`Downloading image for ad ID ${ad.ID} from ${ad.imageSourceUrl}...`)
+        logger.info(`Downloading image for ad ID ${ad.ID} from ${ad.imageSourceUrl}...`)
         let [ downloadResult, error ] = await wg.downloadImage(ad.imageSourceUrl)
         if (downloadResult) { 
             // full image binary is a temporary value. It shouldn't be save to db
             ad.fullImageBinary = downloadResult.binary
             ad.updates.imageExtension = downloadResult.extension
-            console.log(`Downloaded ${downloadResult.extension} image`)
+            logger.info(`Downloaded ${downloadResult.extension} image`)
         } else {
             if (Object.hasOwn(error, 'response') 
                 && Object.hasOwn(error.response, 'status')
@@ -80,11 +85,11 @@ async function main() {
                 if (ad.numOfTries >= MAX_NUM_OF_DOWNLOAD_ATTEMPTS) {
                     ad.updates.failedToDownLoad = true
                 }
-                console.log(`Could not download (status code: ${error.response.status}). Will try later. Attempts: ${numOfTries}`)
+                logger.info(`Could not download (status code: ${error.response.status}). Will try later. Attempts: ${numOfTries}`)
             } else {
                 // stop downloading attempts if received this flag
                 ad.updates.failedToDownLoad = true
-                console.log(`Failed to download`) 
+                logger.info(`Failed to download`) 
             }
         ad.updates.error = JSON.stringify(error) // , Object.getOwnPropertyNames(error))
         }
@@ -111,7 +116,7 @@ async function main() {
                 'fill',
                 false)
             ad.updates.imageForPixelMap = imageBuffer // null if error
-            console.log(`Created image for pixel map`)
+            logger.info(`Created image for pixel map`)
 
             // single write of error for both resizes
             if ( error ) {
@@ -123,7 +128,7 @@ async function main() {
     }
     let [ updatesCount , updateError ] = await db.appendImagesToAds(ads)
     await db.close()
-    console.log(`Updated ${updatesCount} images in the db`)
+    logger.info(`Updated ${updatesCount} images in the db`)
 }
 
 
