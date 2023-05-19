62e015886bcdab84d68f92b9f82ba7ed171589b5
Petr Porobov <porobov@users.noreply.github.com>
2023-05-19 13:18:48 +0400

    Chain now accepts an instance of contract as well as an option

--- src/chain.js
+++ src/chain.js
@@ -3,15 +3,21 @@ const { logger } = require("./logger.js")
 
 const MODULE_NAME = "chain"
 class MillionEther {
-  constructor(contractName, contractAddress){
+  constructor(contractName, contractAddress, contractInstance = null){
     this.contractAddress = contractAddress
     this.contractName = contractName
+    this.contractInstance = contractInstance
   }
 
   async getEvents(eventName, fromBlock) {
     try {
+      let contract
+      if (this.contractInstance) {
+        contract = this.contractInstance
+      } else {
+        contract = await ethers.getContractAt(this.contractName, this.contractAddress)
+      }
       const latestBlock = (await ethers.provider.getBlock("latest")).number
-      let contract = await ethers.getContractAt(this.contractName, this.contractAddress)
       const eventFilter = contract.filters[eventName]()
       const events = await contract.queryFilter(eventFilter, fromBlock, latestBlock)
       return {
